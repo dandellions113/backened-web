9aff21346c730fc8f7e99cd27b50d2ab5d1c62aa
Ramkalyan7 <yramkalyan0707@gmail.com>
2023-09-28 14:45:23 +0530

    added edit profile

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,36 +1,21 @@
 import { Schema, model } from "mongoose";
 
 const articleSchema = new Schema({
-  Headline: {
+  description: String,
+  lang: String,
+  "published date": Date,
+  publisher: {
+    title: String,
+    href: String,
+  },
+  sentiment: String,
+  time: String,
+  title: {
     type: String,
     required: true,
     unique: true,
   },
-  Description: {
-    type: String,
-  },
-  Img: {
-    type: String, // URL for the article's image
-  },
-  Link: {
-    type: String, // URL for the full article
-  },
-  Timestamp: {
-    type: String, // Timestamp for the article
-  },
-  cms: {
-    type: Boolean, // Indicates if the article is from a content management system
-  },
-  content: {
-    type: String, // Full content of the article
-  },
-  sentiment: {
-    type: String, // Sentiment of the article (e.g., "positive," "negative," "neutral")
-  },
-  time: {
-    type: Number, // Unix timestamp or any other relevant time format
-  },
-  // Add other fields as needed
+  url: String,
 });
 
 export default model("Article", articleSchema);
